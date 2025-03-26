6fdd47cbec82ab7dc53672d1774d69b1e91432fe
leon8lion <leon.vasington@students.mq.edu.au>
2025-03-26 14:35:42 +1100

    got mark for week 1

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -6,8 +6,12 @@ uniform vec2 u_screenSize;  	// screen dimensions in pixels
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 p = gl_FragCoord.xy / (u_screenSize);   // scale p into range (0,0) to (1,1)
+   //float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   
+   //vec2 v = abs(p - vec2(0.95, 0.95));   
+   
+    float d = abs(p.x - 0.5) + abs(p.y - 0.5);
   
    if (d < 0.5) {
       o_colour = vec4(u_colour, 1);
